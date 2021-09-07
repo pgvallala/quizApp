029bf494637ec9c574e67762fe60cdcec96fe252
pranav157 <pranavgupta.v@gmail.com>
2021-09-07 20:47:50 +0200

    formatted code

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const quizData = [
-    {
-        question: "What is CSS?",
-        a: "Computer Security Systems",
-        b: "Captivating Style Sheet",
-        c: "Cascading Style Sheet",
-        d: "Central Suspended State",
-        correct: "c",
-    },
+  {
+    question: "What is CSS?",
+    a: "Computer Security Systems",
+    b: "Captivating Style Sheet",
+    c: "Cascading Style Sheet",
+    d: "Central Suspended State",
+    correct: "c",
+  },
 ];
 
-const quiz = document.getElementById('quiz');
-const answerEls = document.querySelectorAll('.answer');
-const questionEl = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
 let score = 0;
@@ -24,52 +24,51 @@ let score = 0;
 loadQuiz();
 
 function loadQuiz() {
-    deselectAnswers();
+  deselectAnswers();
 
-    const currentQuizData = quizData[currentQuiz];
+  const currentQuizData = quizData[currentQuiz];
 
-    questionEl.innerHTML = currentQuizData.question;
-    a_text.innerHTML = currentQuizData.a;
-    b_text.innerHTML = currentQuizData.b;
-    c_text.innerHTML = currentQuizData.c;
-    d_text.innerHTML = currentQuizData.d;
+  questionEl.innerHTML = currentQuizData.question;
+  a_text.innerHTML = currentQuizData.a;
+  b_text.innerHTML = currentQuizData.b;
+  c_text.innerHTML = currentQuizData.c;
+  d_text.innerHTML = currentQuizData.d;
 }
 
 function deselectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false);
-
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
 
 function getSelected() {
-    let answer;
+  let answer;
 
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            answer = answerEl.id;
-        }
-    })
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
 
-    return answer;
+  return answer;
 }
 
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected();
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
 
-    if(answer) {
-        if(answer === quizData[currentQuiz].correct) {
-            score++;
-        }
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
 
-        currentQuiz++
+    currentQuiz++;
 
-        if(currentQuiz < quizData.length) {
-          loadQuiz();
-        } else {
-            quiz.innerHTML = `
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
             <h2>You answered ${score}/${quizData.length} questions correctly</h2>
 
             <button onclick="location.reload()">Wanna try again?</button>
-            `
-        }
+            `;
     }
-})
+  }
+});
